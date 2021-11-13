b3b4ebbc94a45ea1df5b76beac239ffeb940f029
kandivalasa prasad <kandivalasa.prasad97@gmail.com>
2021-11-13 12:09:23 +0530

    Added : employee present or absent programme

---
+++ src/com/bridgelabz/EmployeePayRoll/EmployeeAttendence.java
@@ -0,0 +1,16 @@
+package com.bridgelab.EmployeePayroll;
+
+public class EmployeeAttendence {
+
+	public static void main(String[] args) {
+
+		int present = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == present) {
+			System.out.println("Employee is present");
+		} else
+			System.out.println("Employee is Absent");
+
+	}
+
+}
